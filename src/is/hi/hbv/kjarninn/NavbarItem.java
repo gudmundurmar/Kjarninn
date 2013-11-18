d75bdd98a1b12c9ce81b2f193041e3063c7b7e2c
CyborgPatrick <cyborgpatrick@gmail.com>
2013-11-18 14:49:44 +0000

    Revert ab98d4c..ec8e0b3
    This rolls back to commit ab98d4c573be6bee02b74293860f6790b9336c56.

---
+++ src/is/hi/hbv/kjarninn/NavbarItem.java
@@ -0,0 +1,17 @@
+package is.hi.hbv.kjarninn;
+
+public class NavbarItem {
+
+    public int Id;
+    public String IconFile;
+    public String Name;
+
+    public NavbarItem(int id, String iconFile, String name) {
+
+        Id = id;
+        IconFile = iconFile;
+        Name = name;
+
+    }
+
+}
