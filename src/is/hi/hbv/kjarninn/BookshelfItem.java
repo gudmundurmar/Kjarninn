75bd521c56e5583158b5426e359276a387fc2bcf
CyborgPatrick <cyborgpatrick@gmail.com>
2013-11-18 14:46:11 +0000

    eitthvað drasl ekki nota
    This reverts commit e5f676a0904accbb6fe591bc8e721bdc88cdcd97.

---
+++ src/is/hi/hbv/kjarninn/BookshelfItem.java
@@ -0,0 +1,24 @@
+package is.hi.hbv.kjarninn;
+
+public class BookshelfItem {
+
+    public int Id;
+    public String IconFile;
+    public String Title;
+    public String Headline;
+    public String Date;
+    public String Buttontext;
+
+    public BookshelfItem(int id, String iconFile, String title, String headline, String date, String buttontext) {
+
+        Id = id;
+        IconFile = iconFile;
+        Title = title;
+        Headline = headline;
+        Date = date;
+        Buttontext = buttontext;
+        
+
+    }
+
+}
